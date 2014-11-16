d757e0a7e83760143577b9820f584d1cd5767948
Christopher <christopher@christophers-macbook.local>
2014-11-16 00:31:41 +0000

    Initial Commit

---
+++ Crypto/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Crypto
+//
+//  Created by Christopher on 16/11/2014.
+//  Copyright (c) 2014 Christopher Bell. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
