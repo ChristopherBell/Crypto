bd5b2944f372a44c570b73db15d9ac116a922fbf
Christopher Bell <christopher@christophers-macbook.local>
2014-11-26 00:25:59 +0000

    A very simple example of using the CryptoSwift framework for hashing strings.

--- Crypto/main.swift
+++ Crypto/main.swift
@@ -7,6 +7,32 @@
 //
 
 import Foundation
+import CryptoSwift
 
-println("Hello, World!")
+// MARK: Hashes
 
+var data: NSData = NSData(bytes: "Hello Swift", length: 11)
+
+if let data = CryptoSwift.Hash.md5(data).calculate() {
+    println("\nmd5 -> \(data.hexString)")
+}
+
+if let data = CryptoSwift.Hash.sha1(data).calculate() {
+    println("\nsha1 -> \(data.hexString)")
+}
+
+if let data = CryptoSwift.Hash.sha224(data).calculate() {
+    println("\nsha224 -> \(data.hexString)")
+}
+
+if let data = CryptoSwift.Hash.sha256(data).calculate() {
+    println("\nsha256 -> \(data.hexString)")
+}
+
+if let data = CryptoSwift.Hash.sha384(data).calculate() {
+    println("\nsha384 -> \(data.hexString)")
+}
+
+if let data = CryptoSwift.Hash.sha512(data).calculate() {
+    println("\nsha512 -> \(data.hexString)")
+}
